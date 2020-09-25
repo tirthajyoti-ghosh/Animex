02c65d00d5e2cefca6152ec09d9457a031ed74e3
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-09-26 01:06:42 +0530

    Updates constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 const CORS_HANDLER = 'https://cors-anywhere.herokuapp.com/';
 export const BASE_URL = `${CORS_HANDLER}https://api.jikan.moe/v3/`;
-export const BASE_SEARCH_URL = `${CORS_HANDLER}https://api.jikan.moe/v3/search/anime?`;
+export const BASE_SEARCH_URL = `${BASE_URL}search/anime?`;
+export const ANIME_DETAILS_URL = `${BASE_URL}anime/`;
 
 export const ADD_TOP_ANIME = 'ADD_TOP_ANIME';
