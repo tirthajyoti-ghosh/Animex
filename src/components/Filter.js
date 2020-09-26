7c0e8b0d9fa0db9b904f4043727d2985e08420f9
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-09-26 21:43:44 +0530

    Add more type in Filter component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,8 +4,9 @@ const Filter = ({ handleFilterChange }) => (
   <select onChange={e => { handleFilterChange(e.target.value); }}>
     <option value="All" key="All">All</option>
     <option value="TV" key="TV">TV</option>
-    <option value="Movie" key="Movie">Movie</option>
     <option value="OVA" key="OVA">OVA</option>
+    <option value="Special" key="Special">Special</option>
+    <option value="ONA" key="ONA">ONA</option>
   </select>
 );
 
