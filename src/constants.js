48621728752a2687708343f493a3d09495cd3551
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-09-27 13:14:47 +0530

    Update search url and add genre search url

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,10 @@
 const CORS_HANDLER = 'https://cors-anywhere.herokuapp.com/';
 const BASE_URL = `${CORS_HANDLER}https://api.jikan.moe/v3/`;
+const BASE_SEARCH_URL = `${BASE_URL}search/anime?`;
 
-export const ANIME_LIST_URL = `${BASE_URL}search/anime?rated=pg13&order_by=score`;
+export const ANIME_LIST_URL = `${BASE_SEARCH_URL}rated=pg13&order_by=score`;
 export const ANIME_DETAILS_URL = `${BASE_URL}anime/`;
+export const GENRE_SEARCH_URL = `${BASE_SEARCH_URL}status=airing&order_by=score&genre=`;
 
 export const ADD_ANIME_LIST = 'ADD_ANIME_LIST';
 export const ADD_ANIME_DETAILS = 'ADD_ANIME_DETAILS';
