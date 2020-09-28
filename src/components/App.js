f5d74d27f161ee72396d0154a584db8762d1ed4c
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-09-28 21:50:48 +0530

    Add hamburger menu package
    - Fix empty main tag when data loads in AnimeDetails

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { push as Menu } from 'react-burger-menu';
 import '../assets/styles/App.sass';
 import AnimeList from '../containers/AnimeList';
 import AnimeDetails from '../containers/AnimeDetails';
@@ -8,10 +9,24 @@ import Navbar from './Navbar';
 
 const App = () => (
   <Router>
-    <Navbar />
-    <Route exact path="/" component={AnimeList} />
-    <Route exact path="/genre/:genreId/:genreName" component={GenreList} />
-    <Route exact path="/anime/:animeId" component={AnimeDetails} />
+    <div id="outer-container">
+      {/* <Route>
+        <Menu pageWrapId="page-wrap" outerContainerId="outer-container">
+          <Link to="/genre/1/Action">Action</Link>
+          <Link to="/genre/4/Comedy">Comedy</Link>
+          <Link to="/genre/22/Romance">Romance</Link>
+          <Link to="/genre/27/Shounen">Shounen</Link>
+          <Link to="/genre/37/Supernatural">Supernatural</Link>
+        </Menu>
+      </Route> */}
+      <div id="page-wrap">
+        <Navbar />
+        <Route exact path="/" component={AnimeList} />
+        <Route exact path="/genre/:genreId/:genreName" component={GenreList} />
+        <Route exact path="/anime/:animeId" component={AnimeDetails} />
+      </div>
+    </div>
+
   </Router>
 );
 
--- src/containers/animeDetails.js
+++ src/containers/animeDetails.js
@@ -25,126 +25,125 @@ const AnimeDetails = ({ animeDetailsAdder, animeDetails, match }) => {
   }, [animeDetails.mal_id]);
 
   return (
-    <main style={{ backgroundImage: `linear-gradient(180deg, rgba(27,27,27,0.3785889355742297) 0%, rgba(27,27,27,0.9640231092436975) 65%), url(${animeDetails.image_url}` }} className="details">
+    <>
       {
         animeDetails.mal_id !== parseInt(animeId, 10) ? <Loading /> : (
-          <div className="anime-details">
-            <div className="anime__img">
-              <img src={animeDetails.image_url} alt={animeDetails.title} />
+          <main style={{ backgroundImage: `linear-gradient(180deg, rgba(27,27,27,0.3785889355742297) 0%, rgba(27,27,27,0.9640231092436975) 65%), url(${animeDetails.image_url}` }} className="details">
+            <div className="anime-details">
+              <div className="anime__img">
+                <img src={animeDetails.image_url} alt={animeDetails.title} />
 
-              <div className="anime__watch-links">
-                <a href={animeDetails.url} className="watch">
-                  <i className="fas fa-play-circle" />
-                  <span>&nbsp; Watch Now</span>
-                </a>
+                <div className="anime__watch-links">
+                  <a href={animeDetails.url} target="_blank" rel="noreferrer" className="watch">
+                    <i className="fas fa-play-circle" />
+                    <span>&nbsp; Watch Now</span>
+                  </a>
+                  {
+                    animeDetails.trailer_url === null
+                      ? (
+                        <p>
+                          <i className="fas fa-exclamation-circle" />
+                          &nbsp;
+                          Trailer not avaliable
+                        </p>
+                      )
+                      : (
+                        <a href={animeDetails.trailer_url} target="_blank" className="trailer">
+                          <i className="fas fa-play-circle" />
+                          <span>&nbsp; View Trailer</span>
+                        </a>
+                      )
+                  }
+                </div>
+              </div>
+
+              <div className="anime-info">
+
+                <h1 className="anime__title">{animeDetails.title}</h1>
+
+                <div className="anime__genres">
+                  {
+                    animeDetails.genres.map(genre => (
+                      <Link
+                        key={genre.mal_id}
+                        to={`/genre/${genre.mal_id}/${genre.name}`}
+                      >
+                        {genre.name}
+                      </Link>
+                    ))
+                  }
+                </div>
+
+
                 {
-                  animeDetails.trailer_url === null
+                  animeDetails.score === null
                     ? (
                       <p>
                         <i className="fas fa-exclamation-circle" />
                         &nbsp;
-                        Trailer not avaliable
+                        Score not avaliable
                       </p>
                     )
                     : (
-                      <a href={animeDetails.trailer_url} className="trailer">
-                        <i className="fas fa-play-circle" />
-                        <span>&nbsp; View Trailer</span>
-                      </a>
+                      <div className="anime__score">
+                        <i className="fas fa-star-half-alt" />
+                        <span>{animeDetails.score}</span>
+                      </div>
+                    )
+                }
+
+                <p>
+                  Type:&nbsp;
+                  {animeDetails.type}
+                </p>
+
+                <p>
+                  Released:&nbsp;
+                  {animeDetails.aired.string}
+                </p>
+
+                {
+                  animeDetails.airing === true
+                    ? (
+                      <p>
+                        Airing:
+                        &nbsp;
+                        <i className="fas fa-check" />
+                      </p>
+                    )
+                    : (
+                      <p>
+                        Airing:
+                        &nbsp;
+                        <i className="fas fa-times" />
+                      </p>
+                    )
+                }
+
+                <p>
+                  Duration:&nbsp;
+                  {animeDetails.duration}
+                </p>
+
+                {
+                  animeDetails.synopsis === null
+                    ? (
+                      <p>
+                        <i className="fas fa-exclamation-circle" />
+                        &nbsp;
+                        Synopsis not avaliable
+                      </p>
+                    )
+                    : (
+                      <p>{animeDetails.synopsis}</p>
                     )
                 }
               </div>
             </div>
-
-            <div className="anime-info">
-
-              <h1 className="anime__title">{animeDetails.title}</h1>
-
-              <div className="anime__genres">
-                {
-                  animeDetails.genres.map(genre => (
-                    <Link
-                      key={genre.mal_id}
-                      to={`/genre/${genre.mal_id}/${genre.name}`}
-                    >
-                      {genre.name}
-                    </Link>
-                  ))
-                }
-              </div>
-
-
-              {
-                animeDetails.score === null
-                  ? (
-                    <p>
-                      <i className="fas fa-exclamation-circle" />
-                      &nbsp;
-                      Score not avaliable
-                    </p>
-                  )
-                  : (
-                    <div className="anime__score">
-                      <i className="fas fa-star-half-alt" />
-                      <span>{animeDetails.score}</span>
-                    </div>
-                  )
-              }
-
-              <p>
-                Type:&nbsp;
-                {animeDetails.type}
-              </p>
-
-              <p>
-                Released:&nbsp;
-                {animeDetails.aired.string}
-              </p>
-
-              {
-                animeDetails.airing === true
-                  ? (
-                    <p>
-                      Airing:
-                      &nbsp;
-                      <i className="fas fa-check" />
-                    </p>
-                  )
-                  : (
-                    <p>
-                      Airing:
-                      &nbsp;
-                      <i className="fas fa-times" />
-                    </p>
-                  )
-              }
-
-              <p>
-                Duration:&nbsp;
-                {animeDetails.duration}
-              </p>
-
-              {
-                animeDetails.synopsis === null
-                  ? (
-                    <p>
-                      <i className="fas fa-exclamation-circle" />
-                      &nbsp;
-                      Synopsis not avaliable
-                    </p>
-                  )
-                  : (
-                    <p>{animeDetails.synopsis}</p>
-                  )
-              }
-
-
-            </div>
-
-          </div>
+          </main>
         )
       }
-    </main>
+    </>
   );
 };
 
