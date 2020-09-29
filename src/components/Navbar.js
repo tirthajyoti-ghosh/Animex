a182b455bec1cb8c16f2be60e0854dd399de4d0f
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-09-29 14:45:47 +0530

    Fix mobile menu and add styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <nav>
+  <nav className="desktop-menu">
     <a className="logo" href="/"><img src="https://fontmeme.com/permalink/200927/7644f67b9ca3547230cb655ba735cc04.png" alt="logo" border="0" /></a>
 
     <ul className="genre-links">
+      <li className="active"><Link to="/">Home</Link></li>
       <li><Link to="/genre/1/Action">Action</Link></li>
       <li><Link to="/genre/4/Comedy">Comedy</Link></li>
       <li><Link to="/genre/22/Romance">Romance</Link></li>
