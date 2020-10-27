70021538ac7d67d92e1a05115478e91393b7ed22
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-10-27 16:47:12 +0530

    Create API file to get featured anime data

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,7 @@ const BASE_SEARCH_URL = `${BASE_URL}search/anime?`;
 export const ANIME_LIST_URL = `${BASE_SEARCH_URL}rated=pg13&order_by=score`;
 export const ANIME_DETAILS_URL = `${BASE_URL}anime/`;
 export const GENRE_SEARCH_URL = `${BASE_SEARCH_URL}rated=pg13&status=airing&order_by=score&genre=`;
+export const FEATURED_ANIME_URL = `${ANIME_LIST_URL}&status=airing&page=${Math.floor(Math.random() * 7) + 1}`;
 
 export const ADD_ANIME_LIST = 'ADD_ANIME_LIST';
 export const ADD_GENRE_LIST = 'ADD_GENRE_LIST';
