b2755f65d30a6e96dffd103348b124b411b6f795
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-10-27 14:06:34 +0530

    Update loading component styles

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Loading = () => (
-  <svg viewBox="25 25 50 50">
-    <circle cx="50" cy="50" r="20" />
-  </svg>
+  <div className="loading">
+    <svg viewBox="25 25 50 50">
+      <circle cx="50" cy="50" r="20" />
+    </svg>
+  </div>
 );
 
 export default Loading;
