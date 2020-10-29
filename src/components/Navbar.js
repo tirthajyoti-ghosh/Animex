b64fa34883639b7eac314a4d4162fa8c8673d1ce
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-10-29 13:10:53 +0530

    Replace Link with NavLink to handle active styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
   <header className="nav-header">
@@ -7,12 +7,12 @@ const Navbar = () => (
       <a className="logo" href="/"><img src="https://fontmeme.com/permalink/200927/7644f67b9ca3547230cb655ba735cc04.png" alt="logo" border="0" /></a>
 
       <ul className="genre-links">
-        <li className="active"><Link to="/">Home</Link></li>
-        <li><Link to="/genre/1/Action">Action</Link></li>
-        <li><Link to="/genre/4/Comedy">Comedy</Link></li>
-        <li><Link to="/genre/22/Romance">Romance</Link></li>
-        <li><Link to="/genre/27/Shounen">Shounen</Link></li>
-        <li><Link to="/genre/37/Supernatural">Supernatural</Link></li>
+        <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
+        <li><NavLink exact to="/genre/1/Action" activeClassName="active">Action</NavLink></li>
+        <li><NavLink exact to="/genre/4/Comedy" activeClassName="active">Comedy</NavLink></li>
+        <li><NavLink exact to="/genre/22/Romance" activeClassName="active">Romance</NavLink></li>
+        <li><NavLink exact to="/genre/27/Shounen" activeClassName="active">Shounen</NavLink></li>
+        <li><NavLink exact to="/genre/37/Supernatural" activeClassName="active">Supernatural</NavLink></li>
       </ul>
 
       <ul className="login-links">
