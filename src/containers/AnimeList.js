6b867d431be0b5e6a4c65547b4be41ff564b780b
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-10-30 01:18:05 +0530

    Remove Filter component

--- src/containers/AnimeList.js
+++ src/containers/AnimeList.js
@@ -3,23 +3,19 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import getAnimeList from '../API/getAnimeList';
 import addAnimeList from '../store/actions/addAnimeList';
-import changeAnimeType from '../store/actions/changeAnimeType';
-import Filter from '../components/Filter';
 import FeaturedAnime from './FeaturedAnime';
 import GenreRow from './GenreRow';
 
 const mapDipatchToProps = dispatch => ({
   animeListAdder: animeArray => dispatch(addAnimeList(animeArray)),
-  handleFilterChange: animeType => dispatch(changeAnimeType(animeType)),
 });
 
 const mapStateToProps = state => ({
   animeList: state.animeList,
-  animeType: state.animeType,
 });
 
 const AnimeList = ({
-  animeListAdder, handleFilterChange, animeType,
+  animeListAdder,
 }) => {
   useEffect(() => {
     getAnimeList()
@@ -33,7 +29,6 @@ const AnimeList = ({
   return (
     <main>
       <FeaturedAnime />
-      <Filter handleFilterChange={handleFilterChange} animeType={animeType} />
 
       <GenreRow genreId="1" genreName="action" />
       <GenreRow genreId="4" genreName="comedy" />
@@ -46,13 +41,6 @@ const AnimeList = ({
 
 AnimeList.propTypes = {
   animeListAdder: PropTypes.func.isRequired,
-  handleFilterChange: PropTypes.func.isRequired,
-  animeType: PropTypes.string.isRequired,
-  animeList: PropTypes.arrayOf(PropTypes.shape({
-    mal_id: PropTypes.number.isRequired,
-    image_url: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-  }).isRequired).isRequired,
 };
 
 export default connect(
