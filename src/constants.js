046622e376dd3ffca6046d843041e390373685ec
Tirthajyoti Ghosh <itirthahere@gmail.com>
2020-11-02 15:16:56 +0530

    Update GENRE_SEARCH_URL in constants

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ const BASE_SEARCH_URL = `${BASE_URL}search/anime?`;
 export const ANIME_LIST_URL = `${BASE_SEARCH_URL}rated=pg13&order_by=score`;
 export const FEATURED_ANIME_URL = `${BASE_SEARCH_URL}rated=pg13&status=airing`;
 export const ANIME_DETAILS_URL = `${BASE_URL}anime/`;
-export const GENRE_SEARCH_URL = `${BASE_SEARCH_URL}rated=pg13&status=airing&order_by=score&genre=`;
+export const GENRE_SEARCH_URL = `${BASE_SEARCH_URL}rated=pg13&order_by=score&genre=`;
 
 export const ADD_ANIME_LIST = 'ADD_ANIME_LIST';
 export const ADD_FEATURED_ANIME = 'ADD_FEATURED_ANIME';
