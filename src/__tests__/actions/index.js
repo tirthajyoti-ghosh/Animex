a3c0164faec5b16e63b9ef4dc85512e818f5700f
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-12-09 14:34:40 +0530

    Add tests for GenreRow and FeauredAnime actions

--- src/__tests__/actions/index.js
+++ src/__tests__/actions/index.js
@@ -1,12 +1,14 @@
 import {
-  ADD_GENRE_LIST, ADD_ANIME_DETAILS,
+  ADD_GENRE_LIST, ADD_ANIME_DETAILS, ADD_GENRE_ROW, ADD_FEATURED_ANIME,
 } from '../../constants';
 
 import addAnimeDetails from '../../store/actions/addAnimeDetails';
 import addGenreList from '../../store/actions/addGenreList';
+import addFeaturedAnime from '../../store/actions/addFeaturedAnime';
+import addGenreRow from '../../store/actions/addGenreRow';
 
 describe('Actions', () => {
-  test('creates an action to add genre list', () => {
+  test('add genre list', () => {
     const expectedResult = {
       type: ADD_GENRE_LIST,
       animeList: [],
@@ -15,12 +17,31 @@ describe('Actions', () => {
     expect(addGenreList([])).toEqual(expectedResult);
   });
 
-  test('creates an action to add anime details', () => {
+  test('add anime details', () => {
     const expectedResult = {
       type: ADD_ANIME_DETAILS,
       animeDetails: {},
     };
 
     expect(addAnimeDetails({})).toEqual(expectedResult);
+  });
+
+  test('add genre row', () => {
+    const expectedResult = {
+      type: ADD_GENRE_ROW,
+      genre: 'action',
+      animeArray: [],
+    };
+
+    expect(addGenreRow('action', [])).toEqual(expectedResult);
+  });
+
+  test('add featured anime', () => {
+    const expectedResult = {
+      type: ADD_FEATURED_ANIME,
+      featuredAnime: {},
+    };
+
+    expect(addFeaturedAnime({})).toEqual(expectedResult);
   });
 });
