c73738e9efec235df37825572797ec96bcd25e74
Tirthajyoti Ghosh <itirthahere@gmail.com>
2021-12-29 19:20:52 +0530

    Complete #32
    Use debounce in Search component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Loading from './Loading';
 import getSearchResult from '../API/getSearchResults';
 
 const SearchBar = () => {
-    const [display, setDisplay] = useState(false);
     const [options, setOptions] = useState([]);
     const [query, setQuery] = useState('');
 
@@ -11,29 +10,30 @@ const SearchBar = () => {
         getSearchResult(searchString)
             .then(result => {
                 setOptions(result);
+            })
+            .catch(err => {
+                console.log(err);
             });
     };
 
-    const beginSearch = e => {
-        const searchString = e.target.value;
+    const isQueryLengthSufficient = query.length > 3; // to avoid unnecessary requests
 
-        setQuery(searchString);
-
-        if (searchString.length > 3) {
-            setDisplay(true);
-
-            setTimeout(() => {
-                parseSearchResult(searchString);
-            }, 500);
+    // Debounce the search query
+    useEffect(() => {
+        let timeout;
+        if (isQueryLengthSufficient) {
+            timeout = setTimeout(() => {
+                parseSearchResult(query);
+            }, 1000);
         } else {
-            setDisplay(false);
             setOptions([]);
         }
-    };
+
+        return () => clearTimeout(timeout);
+    }, [query]);
 
     const endSearch = () => {
         setQuery('');
-        setDisplay(false);
         setOptions([]);
     };
 
@@ -43,10 +43,11 @@ const SearchBar = () => {
                 <path fill="#fff" d="M508.5 481.6l-129-129c-2.3-2.3-5.3-3.5-8.5-3.5h-10.3C395 312 416 262.5 416 208 416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c54.5 0 104-21 141.1-55.2V371c0 3.2 1.3 6.2 3.5 8.5l129 129c4.7 4.7 12.3 4.7 17 0l9.9-9.9c4.7-4.7 4.7-12.3 0-17zM208 384c-97.3 0-176-78.7-176-176S110.7 32 208 32s176 78.7 176 176-78.7 176-176 176z" />
             </svg>
 
-            <input type="text" value={query} onChange={beginSearch} placeholder="Start typing..." />
+            <input type="text" value={query} onChange={e => setQuery(e.target.value)} placeholder="Start typing..." />
 
             {
-                display && (
+                // using derived value from `query` to determine whether to show search items
+                isQueryLengthSufficient && (
                     <>
                         <svg xmlns="http://www.w3.org/2000/svg" className="dismiss" onClick={endSearch} width="15" height="15" viewBox="0 0 320 512">
                             <path fill="#fff" d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z" />
